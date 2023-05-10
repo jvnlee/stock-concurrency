9c5218914491efd84341919daf4b5d44acf0e160
jvnlee <lhjun1028@naver.com>
2023-05-10 18:11:12 +0900

    decrese() 메서드에 synchronized 키워드를 추가
    synchronized 키워드를 추가하면 언어 레벨에서 동기화를 보장해줌. 그러나 @Transactional 어노테이션과 같이 사용하면 AOP 작동 시 동기화가 깨질 수 있기 때문에 주석 처리함.

--- src/main/java/com/example/stockexample/service/StockService.java
+++ src/main/java/com/example/stockexample/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.stockexample.domain.Stock;
 import com.example.stockexample.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +12,8 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
