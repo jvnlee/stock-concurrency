139167909cc0c432e5240501e7cd5b727c9c19fe
jvnlee <lhjun1028@naver.com>
2023-05-11 16:14:02 +0900

    StockRepository에 Pessimistic Lock을 사용한 메서드 추가

--- src/main/java/com/example/stockexample/repository/StockRepository.java
+++ src/main/java/com/example/stockexample/repository/StockRepository.java
@@ -2,6 +2,15 @@ package com.example.stockexample.repository;
 
 import com.example.stockexample.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import static javax.persistence.LockModeType.*;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
+
 }
